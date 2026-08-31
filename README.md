00a8331a4c30f03c4dbe3de5aaa5c554386115b5
boosic <basic6952@gmail.com>
2026-08-31 21:13:37 +0300

    fresh start clean site

---
+++ README.md
@@ -0,0 +1,3 @@
+# [thrills.github.io](https://007gidon.github.io/thrills.github.io/)
+# Need to fix the fucking CSS
+# The site must be opened in 150% ZOOM on chrome
---
+++ scripts/master.js
@@ -0,0 +1,43 @@
+// פונקציית הטריילר 
+function toggleVideo() {
+    const trailer = document.querySelector('.trailer');//בוחר את הקלאס טריילר
+    trailer.classList.toggle('active'); //ממתג את הקלאס 'פעיל' עבור הטריילר
+    document.querySelector('.video').pause(); // עוצר את הסרטון בהתחלה
+}
+
+//  פונקציה זו משנה את הרקע, מעדכנת מקור וידאו ומציגה או מסתירה תוכן על סמך כותרת נתונה
+function changeBG(bg, title, videoSrc) {
+    const videoElement = document.querySelector('.video');//בוחר את הקלאס וידאו
+    const trailer = document.querySelector('.trailer');// בוחר את הקלאס טריילר
+
+    videoElement.pause(); // אלמנט שעוצר את הסרטון
+    document.body.style.cssText = `background: url("${bg}") center/cover;`; // משנה את הרקע, את תמונת הרקע
+    videoElement.src = videoSrc; // מעדכן את המקור של הסרטון
+
+    if (!trailer.classList.contains('active')) {// בוחר את טריילר אם  הוא לא פעיל
+        videoElement.pause(); // עוצר את הסרטון אם הוא לא פעיל
+    }
+
+    
+    document.querySelectorAll('.content').forEach(content => {
+        // בוחרת את הקלאס "CONTENT"
+        content.classList.toggle('active', content.classList.contains(title));
+        // שורה זו משנה את נוכחות המחלקה "פעילה" עבור כל קונטנט אלמנט בהתבסס על האם רשימת הקלאס שלה מכילה את הכותרת שצוינה.
+    });
+}
+
+//  הגדרת פונקצייה בשם מייליסט שמקבלת פרמטר בשם טייטל שכבר נמצאת למעלה
+function addToMyList(title) { 
+    // Clicking "Add to My List" stores the movie title "Martyrs" in the browser's local storage under the key `'myList'`.
+    const myList = JSON.parse(localStorage.getItem('myList')) || []; 
+    // alert(String(myList));
+    if (!myList.includes(title)) { // בדיקה האם הכותרת אינה כבר קיימת ברשימה
+        myList.push(title); // הוספת הכותרת לרשימה אם היא עדיין לא קיימת
+        localStorage.setItem('myList', JSON.stringify(myList)); 
+        // JSONשמירת הרשימה המעודכנת באחסון המקומי לאחר המרה למחרוזת 
+    }
+}
+
+
+
+
