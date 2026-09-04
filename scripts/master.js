3dd47c52d979a87f7611df0060739af324429b6f
boosic <basic6952@gmail.com>
2026-09-04 05:36:41 +0300

    Fix Green Room CSS path and background image

--- scripts/master.js
+++ scripts/master.js
@@ -40,4 +40,13 @@ function addToMyList(title) {
 
 
 
-
+window.addEventListener('DOMContentLoaded', () => {
+    alert(
+        "שימו לב! 🖥️\n\n" +
+        "כדי ליהנות מהחוויה המלאה של האתר, מומלץ בחום:\n" +
+        "• לפתוח במחשב בלבד (לא בפלאפון).\n" +
+        "• להשתמש בדפדפן Google Chrome.\n" +
+        "• לכוון את הזום ל-150%.\n\n" +
+        "צפייה מהנה!"
+    );
+});
